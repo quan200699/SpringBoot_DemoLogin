c0591d5b894aab634f9133578be39fd45871b2d7
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-01-14 21:58:25 +0700

    Update User.java

--- src/main/java/com/codegym/user/model/User.java
+++ src/main/java/com/codegym/user/model/User.java
@@ -25,5 +25,5 @@ public class User implements Serializable {
     @JoinTable(name = "user_role",
             joinColumns = {@JoinColumn(name = "user_id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id")})
-    private Set<Role> roles;
+    private List<Role> roles;
 }
